e1eab1b1c3dd7c79518fc47bd7f73e057fc3b055
dasarimahesh08 <maheshdasarimahesh30@gmail.com>
2026-07-14 22:00:32 +0530

    smtp commit

--- student_project/settings.py
+++ student_project/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-import os
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -57,7 +57,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'student_project.urls'
-
+import os
 TEMPLATE_DIR = os.path.join(BASE_DIR , 'templates')
 STATICFILES_DIRS = [os.path.join(BASE_DIR , 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
@@ -130,5 +130,15 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.getenv("EMAIL_HOST" , "smtp-relay.brevo.com")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER" , "b1f1a9001@smtp-brevo.com")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_PORT = 587
 
+
+RAZORPAY_KEY_ID = "rzp_live_T1Nq64oPe2u5DS"
+RAZORPAY_KEY_SECRET = "am00tmjwm7RVZtrrG6NP1rXH"
 
