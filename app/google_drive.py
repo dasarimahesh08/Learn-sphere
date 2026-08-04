e020faae6e4d544e5f8b6cae577c5ad6dddf308a
dasarimahesh08 <maheshdasarimahesh30@gmail.com>
2026-08-04 17:59:48 +0530

    upload bug fix

--- app/google_drive.py
+++ app/google_drive.py
@@ -28,54 +28,51 @@ def authenticate_google_drive():
         else:
             raise Exception(
                 "No valid Google Drive credentials — check GOOGLE_ACCESS_TOKEN, "
-                "GOOGLE_REFRESH_TOKEN, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET env vars on Render."
-            )
+                "GOOGLE_REFRESH_TOKEN, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET env vars on Render." 
+                )
 
     service = build("drive", "v3", credentials=creds)
     return service
     
 def upload_file_to_drive(uploaded_file):
     service = authenticate_google_drive()
-
     # create temporary file
     temp = tempfile.NamedTemporaryFile(delete=False)
     for chunk in uploaded_file.chunks():
         temp.write(chunk)
-
     temp.close()
-
-    file_metadata = {
-        "name": uploaded_file.name , 
-        "parents": ["1WqJ9x_YsFdzUAce0H7YvkPEmy5XDYMbx"]
-    }
-
-    media = MediaFileUpload(
-    temp.name,
-    mimetype=uploaded_file.content_type,
-    resumable=True
-    )
-
-    request = service.files().create(
-        body=file_metadata,
-        media_body=media,
-        fields="id"
-    )
-
-    response = None
-
-    while response is None:
-        status, response = request.next_chunk()
-        if status:
-            print(f"Uploaded {int(status.progress() * 100)}%")
-
-    service.permissions().create(
-        fileId=response.get("id"),
-        body={
-            "role": "reader",
-            "type": "anyone"
+    try:
+        file_metadata = {
+            "name": uploaded_file.name , 
+            "parents": ["1WqJ9x_YsFdzUAce0H7YvkPEmy5XDYMbx"]
         }
-    ).execute()
-    return response.get("id")
+        media = MediaFileUpload(
+        temp.name,
+        mimetype=uploaded_file.content_type,
+        resumable=True 
+        )
+        request = service.files().create(
+            body=file_metadata,
+            media_body=media,
+            fields="id" 
+            )
+        response = None
+        while response is None:
+            status, response = request.next_chunk()
+            if status:
+                print(f"Uploaded {int(status.progress() * 100)}%")
+        service.permissions().create(
+            fileId=response.get("id"),
+            body={
+                "role": "reader",
+                "type": "anyone"
+            } 
+            ).execute()
+        return response.get("id")
+
+    finally:
+        if os.path.exists(temp.name):
+            os.remove(temp.name)
 
 def get_drive_file_url(file_id):
     return f"https://drive.google.com/file/d/{file_id}/preview?usp=sharing"
